8dc13710d2ababc5e30ab3791b38f37982515abd
Mauro Michelotti <michelotti@bmsitalia.com>
2019-01-03 17:31:30 +0100

    configurazione DB e primo model

---
+++ src/main/java/net/mm/movie/model/Genere.java
@@ -0,0 +1,93 @@
+package net.mm.movie.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "genere", schema = "public", uniqueConstraints = @UniqueConstraint(name = "genere_uk", columnNames = {
+    "codice" }))
+@SequenceGenerator(schema = "public", sequenceName = "genere_seq", name = "genere_seq", allocationSize = 1)
+public class Genere {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "genere_seq")
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(name = "codice", nullable = false)
+  private String codice;
+
+  @Column(name = "descrizione", nullable = false)
+  private String descrizione;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getCodice() {
+    return codice;
+  }
+
+  public void setCodice(String codice) {
+    this.codice = codice;
+  }
+
+  public String getDescrizione() {
+    return descrizione;
+  }
+
+  public void setDescrizione(String descrizione) {
+    this.descrizione = descrizione;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((codice == null) ? 0 : codice.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Genere other = (Genere) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (codice == null) {
+      if (other.codice != null)
+        return false;
+    } else if (!codice.equals(other.codice))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("Genere [id=").append(id);
+    builder.append(", codice=").append(codice);
+    builder.append(", descrizione=").append(descrizione);
+    builder.append("]");
+    return builder.toString();
+  }
+}
---
+++ src/main/java/net/mm/movie/repository/GenereRepository.java
@@ -0,0 +1,15 @@
+package net.mm.movie.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.mm.movie.model.Genere;
+
+@Repository
+public interface GenereRepository extends JpaRepository<Genere, Long> {
+
+  public List<Genere> findByCodice(String codice);
+
+}
